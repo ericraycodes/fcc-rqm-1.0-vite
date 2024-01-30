69b979d0c0417a7a7aeda4ea2e6af11a6f4b6d9d
lasernoy <cloudy.nny07@gmail.com>
2024-01-30 20:30:35 +0800

    App 2.3

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,17 @@ function Footer() {
   window.console.log('<Footer /> RENDER')
   return (
     <footer tabIndex='1'>
-      <p>This app is For freeCodeCamp certification purposes only. By Eric Ray Saladar. 2024.</p>
+      <p>This app is for <a
+          id='freeCodeCamp'
+          rel='noreferrer'
+          href='https://www.freecodecamp.org/learn/'
+          target='_blank'
+        ><u>freeCodeCamp</u></a> certification purposes only. By <a
+          id='developer'
+          rel='noreferrer'
+          href='https://github.com/ericraycodes/fcc-rqm-1.0-vite'
+          target='_blank'
+        ><u>Eric Ray Saladar</u></a>. 2024.</p>
     </footer>
   );
 }
